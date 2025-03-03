88bb729c9da36f7d4a3be708175c45f28156427a
Liaqat Saeed <liaqatsaeed007@gmail.com>
2025-03-03 20:54:45 +0500

    ci(wf issues): file import issues with WF

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ImageProps } from "./image.interface.ts";
-import { imageStyle } from "./image.css.ts";
+import { ImageProps } from "./image.interface";
+import { imageStyle } from "./image.css";
 
 export const Image: React.FC<ImageProps> = ({ alt, image = 'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg' }) => {
   return (
