30cd25f7a2f8abbcdf1def503c0b662d319cfae4
Liaqat Saeed <liaqatsaeed007@gmail.com>
2025-03-03 20:58:38 +0500

    ci(wf issues): delete image component because WF was failing

--- src/components/Image/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ImageProps } from "./image.interface";
-import { imageStyle } from "./image.css";
-
-export const Image: React.FC<ImageProps> = ({ alt, image = 'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg' }) => {
-  return (
-    <img alt={alt} loading="lazy" className={imageStyle} decoding="async" srcSet={image} src={image} />
-  );
-};
