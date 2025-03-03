27d7bc35f5448f9e96dd2b3b6e57c11321a47ec4
junaid-umer <57061092+junaid-umer@users.noreply.github.com>
2025-03-03 19:15:05 +0000

    Deploying to gh-pages from  @ b26e767e36a2e983f3c2ebf2cff4c8ede345f810 🚀

--- assets/text.stories-CvDZTJGe.js
+++
@@ -1,6 +0,0 @@
-import{T as s}from"./index-B8AQXhD0.js";import"./jsx-runtime-Yb_4y5hn.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";/* empty css                             */import"./createRuntimeFn-62c9670f.esm-BkdTE7RR.js";import"./index-BpvXyOxN.js";const p={title:"DesignSystem/OnlineFreeCV/Text",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{name:"variant",control:"select",type:{name:"string",required:!0},options:["display","h1","h2","h3","h4","h5","h6","body1","body2","body3"],table:{type:{summary:"Has 10 variants: 7 for headings, 3 for body text",detail:"display | h1 | h2 | h3 | h4 | h5 | h6 | body1 | body2 | body3"},defaultValue:{summary:"body1"}}},className:{name:"className",control:"text",description:"Classname that can be generated using `vanilla-extract/css` or `vanilla-extract/recipe` with the help of `classnames` package.",table:{category:"customization"}}}},a={args:{variant:"h1",children:"This is Heading 1"}};a.name="H1";var e,t,r;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    variant: 'h1',
-    children: "This is Heading 1"
-  }
-}`,...(r=(t=a.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const h=["TextVariants"];export{a as TextVariants,h as __namedExportsOrder,p as default};
