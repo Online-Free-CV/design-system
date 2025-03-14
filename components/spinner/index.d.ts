982e79b26aecb393dd10459470044cd879f2b669
MuhammadHassanAmin <mhassanamin94@gmail.com>
2025-03-14 18:43:32 +0000

    Deploying to gh-pages from  @ 07dc5410cddc313d756692e4c1b96fa92a431e99 🚀

---
+++ components/spinner/index.d.ts
@@ -0,0 +1,4 @@
+import { default as React } from 'react';
+import { SpinnerProps } from './spinner.interface';
+declare const Spinner: React.FC<SpinnerProps>;
+export default Spinner;
---
+++ components/spinner/spinner.stories.d.ts
@@ -0,0 +1,6 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { default as Spinner } from './index';
+declare const meta: Meta<typeof Spinner>;
+export default meta;
+type Story = StoryObj<typeof Spinner>;
+export declare const Default: Story;
