a3f06c662f7052795771f2e10b04582cc711f9cf
LiaqatSaeed <liaqatsaeed007@gmail.com>
2025-04-29 18:45:20 +0000

    Deploying to gh-pages from  @ c4ce69562c54caf7c06058120ea33c659aad86cb 🚀

--- hoc/withFormik.d.ts
+++ hoc/withFormik.d.ts
@@ -1,8 +1,9 @@
+import { FormikConfig, FormikHelpers } from 'formik';
 import * as React from "react";
 interface WithFormikProps<T> {
     initialValues: T;
-    onSubmit: (values: T, actions: any) => void;
-    validationSchema: any;
+    onSubmit: (values: T, actions: FormikHelpers<T>) => void;
+    validationSchema?: FormikConfig<T>["validationSchema"];
     children?: React.ReactNode;
 }
 export declare const withFormik: <T extends object>(WrappedComponent: React.FC<{
