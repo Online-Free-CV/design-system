dab6e811814e6d838f9100d2123d79655f084ff2
LiaqatSaeed <liaqatsaeed007@gmail.com>
2025-06-03 18:34:52 +0000

    Deploying to gh-pages from  @ a1f352fd601ce63fd1e1db71c945c77922ee1cbf 🚀

---
+++ components/colorPicker/color-picker.css.d.ts
@@ -0,0 +1,6 @@
+export declare const dropdownContainer: string;
+export declare const dropdownButton: string;
+export declare const colorSwatch: string;
+export declare const dropdownMenu: string;
+export declare const dropdownItem: string;
+export declare const selectedItem: string;
---
+++ components/colorPicker/index.d.ts
@@ -0,0 +1,6 @@
+import { default as React } from 'react';
+interface ColorPickerProps {
+    name: string;
+}
+export declare const ColorPickerDropdown: React.FC<ColorPickerProps>;
+export {};
